5a2eae924c1a3487cc80415a2ed0e91027aeb79f
agi0917 <peter.vonrohr@gmail.com>
2016-02-22 16:00:19 +0100

    Added slide creation function

--- R/slides_tools.R
+++ R/slides_tools.R
@@ -6,3 +6,18 @@
 ###
 ### ##################################### ###
 
+#' Create a deck of slides
+#'
+#' @description
+#' This is a wrapper function to \code{rmarkdown::draft} that
+#' creates a source document for a deck of beamer slides. The
+#' template is taken from this packages template directory
+#'
+#' @param psSlidesName   Name of Source-File for Slides
+#' @param psCourseDir    Course directory
+#' @export create_slides
+create_slides <- function(psSlidesName, psCourseDir = ".", psSlidesSubdir = NULL){
+  ### # use rmarkdown draft function to copy over the draft file
+  rmarkdown::draft(file = psSlidesName, template = "beamer_slides", package = "rcoursetools")
+
+}
