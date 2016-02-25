69e3e229bbbcc4fc095c25b57f6913581aee0e1c
charlotte-ngs <ngs.charlotte@gmail.com>
2016-02-25 07:17:25 +0100

    Added work around function to generate necessary tex statements for re-scaling plots

--- R/diagramtools.R
+++ R/diagramtools.R
@@ -6,6 +6,14 @@
 ###
 ### ######################################## ###
 
+#' Create an empty diagram
+#'
+#' @description
+#' \code{create_diagram} uses a templated for a given format
+#' that is expected to be stored in inst/extdata/templates
+#'
+#' @param psFormat   Format of diagram to be created
+#' @export create_diagram
 create_diagram <- function(psFormat = "odg"){
   ### # get default settings for diagrams
   lDiagramDefaults <- lGetDiagramDefaults()
@@ -27,4 +35,26 @@ create_diagram <- function(psFormat = "odg"){
 lGetDiagramDefaults <- function(){
   return(list(sTemplatePath = system.file(file.path("inst","extdata","templates"), package = "rcoursetools")
               ))
+}
+
+#' Insert a plot into a document using a given width scale
+#'
+#' @description
+#' \code{genericScaledPlot} is a work-around due to the issue with setkeys{Gin}
+#' which, we have to use because of the background picture in the header and which
+#' has to be specified before each plot because otherwise, the plot is not visible.
+#'
+#' @details
+#' The function \code{genericScaledPlot} can also be used to just produce the
+#' LaTeX statement that sets the Gin key. This can be done when leaving
+#' pfPlotMethod NULL.
+#'
+#' @param pData               data to be plotted
+#' @param pnPaperWidthScale   scale factor for graphics width
+#' @param pfPlotMethod        function that should produce the plot
+#' @export genericScaledPlot
+genericScaledPlot <- function(pData = NULL, pnPaperWidthScale, pfPlotMethod = NULL, ...){
+  cat("\\setkeys{Gin}{width=", pnPaperWidthScale, "\\paperwidth}\n", sep = "")
+  if (!is.null(pfPlotMethod))
+    pfPlotMethod(pData, ...)
 }
--- R/slidestools.R
+++ R/slidestools.R
@@ -11,21 +11,34 @@
 #' @description
 #' This is a wrapper function to \code{rmarkdown::draft} that
 #' creates a source document for a deck of beamer slides. The
-#' template is taken from this packages template directory
+#' template is taken from the template directory of this packages
 #'
 #' @details
 #' The setup here is done analogeously to \code{devtools::use_vignette},
 #' except that here, we are using different templates and we want to
 #' create pdf_ouput and not html_vignette. The template to be used
 #' can be specified as a parameter, by default "beamer_slides" is
-#' used as a template.
+#' used as a template. We explicitly allow and encourage source files of
+#' slides to be organized in subdirectories of the vignettes directory,
+#' knowing that this breaks vignette building by the devtools infrastructure.
+#' There are several reasons for the subdirectories, first the function
+#' \code{rmarkdown::draft} gives an error when a file or directory that
+#' should be copied from the templated directory already exists in the
+#' target directory where it should be copied to. Secondly, we want to
+#' have the possibilities to use different documentclasses for different
+#' slide documents. Thirdly, putting all slide sources of a complete
+#' course in a single directory clutters the whole directory. Hence
+#' organizing the material in subdirectories helps us to keep a better
+#' overview over the whole course material.
 #'
-#' @param psSlidesName       Name of Source-File for Slides
-#' @param psCourseDir        Course directory
+#' @param psSlidesName       name of slides source file
+#' @param psCourseDir        course directory
+#' @param psSlidesSubdir     subdirectory for current slides
 #' @param psSlidesTemplate   template for slides which should be used
 #' @param psSlidesFormat     extension of final slides file name
 #' @export create_slides
 create_slides <- function(psSlidesName, psCourseDir = ".",
+                          psSlidesSubdir = NULL,
                           psSlidesTemplate = "beamer_slides",
                           psSlidesFormat   = "Rmd"){
   ### # doing a setup similar to devtools::use_vignette
@@ -33,9 +46,18 @@ create_slides <- function(psSlidesName, psCourseDir = ".",
   devtools:::add_desc_package(pkg, "Suggests", "knitr")
   devtools:::add_desc_package(pkg, "Suggests", "rmarkdown")
   devtools:::add_desc_package(pkg, "VignetteBuilder", "knitr")
-  dir.create(file.path(pkg$path, "vignettes"), showWarnings = FALSE)
+  ### # setup the directory structure where we want to put the slides
+  sVigDir <- file.path(pkg$path, "vignettes")
+  if (!is.null(psSlidesSubdir)){
+    sSlidesDir <- file.path(sVigDir, psSlidesSubdir)
+  } else {
+    sSlidesDir <- sVigDir
+  }
+  ### # check whether slides directory exists
+  if (!dir.exists(sSlidesDir))
+    dir.create(sSlidesDir, recursive = TRUE, showWarnings = FALSE)
   ### # depending on psSlidesFormat, put different extensions to final file
-  sSlidesPath <- file.path(pkg$path, "vignettes", paste(psSlidesName, "Rmd", sep = "."))
+  sSlidesPath <- file.path(sSlidesDir, paste(psSlidesName, "Rmd", sep = "."))
   ### # use rmarkdown draft function to copy over the draft file
   rmarkdown::draft(file = sSlidesPath,
                    template = psSlidesTemplate,
@@ -43,6 +65,7 @@ create_slides <- function(psSlidesName, psCourseDir = ".",
                    create_dir = FALSE,
                    edit = FALSE)
   sFinalSlideName <- sSlidesPath
+  ### # allow for legacy Rnw based templates
   if (psSlidesFormat == "Rnw"){
     sFinalSlideName <- gsub("Rmd$", "Rnw", sSlidesPath)
     file.rename(from = sSlidesPath, to = sFinalSlideName)
