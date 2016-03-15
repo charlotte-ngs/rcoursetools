f0f6f113e70c92b974dc6419488190d23b80defb
agi0917 <peter.vonrohr@gmail.com>
2016-03-15 12:34:14 +0100

    changed name of insertOdgFigureAsPdf to insertOdgAsPdf. In case pdf-version of graphics are already generated, they are not re-generated by default

--- R/graphicsconverter.R
+++ R/graphicsconverter.R
@@ -25,29 +25,40 @@ convertLibOToPdf <- function(psLibOFile, psLibODir = "odg", psFigOutDir = "."){
 }
 
 
-## ---- Insert a Odg draw figure -------------------------------------------
-#' Inserts an odg draw figure into a rmarkdown text
+## ---- Insert a Odg draw graphic -------------------------------------------
+#' Inserts an odg draw graphic into a rmarkdown text
 #'
-#' \code{insertOdgFigure} takes the name of a file containing a figure
+#' @description
+#' \code{insertOdgAsPdf} takes the name of a file containing a graphic
 #' in odg format, converts the content of that file into pdf using
 #' function \code{convertLibOToPdf} and outputs the string in markdown
-#' format to include the figure
+#' format to include the figure. Pdf-formatted graphics are only re-generated
+#' if the pdf-file does not exist or, if the flag pbMustGenerate is TRUE.
 #'
-#' @param  psOdgFileStem  stem of odg figure file
-#' @param  psOdgDir       directory where odg figure file is stored
-#' @param  psFigOutDir    directory where output should be placed
-#' @export insertOdgFigureAsPdf
-insertOdgFigureAsPdf <- function(psOdgFileStem, psOdgDir = "odg", psFigOutDir = ".", pnPaperWidthScale = NULL) {
-  ### # check that psOdgFileName exists
-  sOdgFilename <- paste(psOdgFileStem, "odg", sep = ".")
-  sOdgFile <- file.path(psOdgDir, sOdgFilename)
-  if (!file.exists(sOdgFile))
-    stop("Cannot find Odg figure file: ", sOdgFile)
-  ### # convert odg file to pdf
-  convertLibOToPdf(psLibOFile = sOdgFilename, psLibODir = psOdgDir, psFigOutDir = psFigOutDir)
-  ### # check that generated pdf file exists
+#' @param  psOdgFileStem    stem of odg figure file
+#' @param  psOdgDir         directory where odg figure file is stored
+#' @param  pbMustGenerate   flag to indicate whether pdf-graphics must be regenerated
+#' @param  psFigOutDir      directory where output should be placed
+#' @export insertOdgAsPdf
+insertOdgAsPdf <- function(psOdgFileStem, psOdgDir = "odg",
+                                 psFigOutDir = ".",
+                                 pbMustGenerate = FALSE,
+                                 pnPaperWidthScale = NULL) {
+  ### # check wether pdf file already exists, if so, do nothing
   sPdfFilename <- paste(psOdgFileStem, "pdf", sep = ".")
   sPdfFile <- file.path(psFigOutDir,sPdfFilename)
+  if (!file.exists(sPdfFile) | pbMustGenerate){
+    ### # if pdf files cannot be found, regenerate them, check that psOdgFileName exists
+    sOdgFilename <- paste(psOdgFileStem, "odg", sep = ".")
+    sOdgFile <- file.path(psOdgDir, sOdgFilename)
+    if (!file.exists(sOdgFile))
+      stop("Cannot find Odg figure file: ", sOdgFile)
+    ### # convert odg file to pdf
+    convertLibOToPdf(psLibOFile = sOdgFilename, psLibODir = psOdgDir, psFigOutDir = psFigOutDir)
+
+  }
+  ### # at this point the pdf file must exist, either from previous conversion
+  ### #  or from converting it right now
   if (!file.exists(sPdfFile))
     stop("Cannot find pdf figure file: ", sPdfFile)
   ### # in case a width scale was specified, use it
